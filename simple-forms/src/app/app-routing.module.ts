7e17018179c4fa01f002300452c1cd0f93b01364
FIRST_NAME LAST_NAME <gitdevjay@gmail.com>
2022-11-01 18:45:06 +0530

    routing done

--- simple-forms/src/app/app-routing.module.ts
+++ simple-forms/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RformComponent } from './rform/rform.component';
+import { TformComponent } from './tform/tform.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo:"rform", pathMatch:"full"}, //there isno route
+  {path:"rform", component:RformComponent},
+  {path:"tform", component:TformComponent},
+  {path:"**", component:RformComponent} // when non of the paths not matching 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
