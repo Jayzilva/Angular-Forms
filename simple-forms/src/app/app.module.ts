c6c6fc37ddfcce2c7c7f3a38eaaebdc04d2213b3
FIRST_NAME LAST_NAME <gitdevjay@gmail.com>
2022-11-01 16:44:05 +0530

    update

--- simple-forms/src/app/app.module.ts
+++ simple-forms/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RformComponent } from './rform/rform.component';
+import { TformComponent } from './tform/tform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RformComponent,
+    TformComponent
   ],
   imports: [
     BrowserModule,
